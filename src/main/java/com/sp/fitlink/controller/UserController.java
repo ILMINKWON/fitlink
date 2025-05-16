2c3f3122a0bc9b912c644fea3e1957214e409b91
ILMINKWON <rnjsdlfals@naver.com>
2025-05-16 18:46:40 +0900

    first commit

---
+++ src/main/java/com/sp/fitlink/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.sp.fitlink.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("fitLink")
+public class UserController {
+
+    @RequestMapping("/mainPage")
+    public String mainPage(){
+
+        return "/user/mainPage";
+        
+    }
+    
+}
